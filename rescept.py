d959863d6d749a6fa5fe6b2d70ea8a0b787d1b75
Grigorev.Anton <ayg97@mail.ru>
2024-01-11 21:43:14 +0300

    Vtoroe Zadanie

--- rescept.py
+++ rescept.py
@@ -28,11 +28,31 @@ def read_recipe_file(file_path):
     
     return recipes
 
-file_path = 'recipes.txt'  
+def get_shop_list_by_dishes(dishes, person_count, cook_book):
+    shop_list = {}
+
+    for dish in dishes:
+        if dish in cook_book:
+            for ingredient in cook_book[dish]:
+                ingredient_name = ingredient['ingredient_name']
+                quantity = ingredient['quantity'] * person_count
+                measure = ingredient['measure']
+
+                if ingredient_name not in shop_list:
+                    shop_list[ingredient_name] = {'measure': measure, 'quantity': quantity}
+                else:
+                    shop_list[ingredient_name]['quantity'] += quantity
+
+    return shop_list
+
+# Пример использования
+file_path = 'recipes.txt'
 cook_book = read_recipe_file(file_path)
 
-for recipe, ingredients in cook_book.items():
-    print(f'{recipe}:')
-    for ingredient in ingredients:
-        print(f"  {ingredient['ingredient_name']}: {ingredient['quantity']} {ingredient['measure']}")
-    print()
+dishes_to_cook = ['Запеченный картофель', 'Омлет']
+person_count = 2
+result_shop_list = get_shop_list_by_dishes(dishes_to_cook, person_count, cook_book)
+
+# Выводим полученный словарь
+for ingredient, info in result_shop_list.items():
+    print(f'{ingredient}: {info["quantity"]} {info["measure"]}')
